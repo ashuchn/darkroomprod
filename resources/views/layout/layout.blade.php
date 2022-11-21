1d50d9fc308c62e906260262e5cae3dc75253f5a
ashuchn <ashutoshchauhan129@gmail.com>
2022-11-22 00:47:23 +0530

    dynamic title

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,12 +6,11 @@
 <head>
         
         <meta charset="utf-8" />
-        <title>Dashboard || DarkroomProd</title>
+        <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Photographers" name="description" />
         <meta content="Themesdesign" name="author" />
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ url('assets/dashboard-nazox/assets/images/favicon.ico') }}">
 
